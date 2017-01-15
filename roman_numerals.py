34c10650beb5bed94a7a5b8f3b5d7b6a36cacc96
bridgetree <rahultach@gmail.com>
2017-01-15 13:18:46 -0500

    Create roman_numerals.py

---
+++ roman_numerals.py
@@ -0,0 +1,22 @@
+# Convert numbers into Roman numerals
+
+def checkio(number):
+    ROMANS = (('M',  1000),
+          ('CM', 900),
+          ('D',  500),
+          ('CD', 400),
+          ('C',  100),
+          ('XC', 90),
+          ('L',  50),
+          ('XL', 40),
+          ('X',  10),
+          ('IX', 9),
+          ('V',  5),
+          ('IV', 4),
+          ('I',  1))
+    sum = ""      
+    for roman, value in ROMANS:
+         while number >= value:
+             number -= value
+             sum += roman
+    return sum
